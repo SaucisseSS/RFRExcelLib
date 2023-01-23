0ffe04c06df99e0a16b5d80f5e9f78cc0a596cbf
SaucisseSS <58030235+SaucisseSS@users.noreply.github.com>
2023-01-23 13:53:06 +0300

    Added project

---
+++ RFRExcelLib/Controllers/ExcelFileController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+
+namespace Controllers.RFRExelLib
+{
+    public sealed class ExcelFileController
+    {
+        private Excel.Application _application;
+        private List<Excel.Workbook> _workbooks;
+
+        public List<Excel.Workbook> Workbooks => _workbooks;
+
+        public ExcelFileController()
+        {
+            _application = new Excel.Application();
+        }
+        public ExcelFileController(string path)
+        {
+            _application = new Excel.Application();
+            _workbooks.Add(_application.Workbooks.Add(path));
+        }
+
+        public void AddWorkBook(string path)
+        {
+            if (path != null) _workbooks.Add(_application.Workbooks.Add(path));
+            else _workbooks.Add(_application.Workbooks.Add(Missing.Value));                   
+        }
+
+
+
+    }
+}
---
+++ RFRExcelLib/Controllers/TableController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Controllers.RFRExcelLib
+{
+    public class TableController
+    {
+        private int _TableHeight { get; set; }
+        private int _TableWidth { get; set; }
+        private string _TablePosition { get; set; }
+        public TableController() { }
+        
+        public TableController(int height, int width)
+        {
+            _TablePosition = "A1";
+            _TableHeight = height;
+            _TableWidth = width;
+        }
+
+        public void NewTable(string path)
+        {
+
+        }
+    }
+}
