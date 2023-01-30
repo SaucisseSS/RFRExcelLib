b47c21efde2a9a3ad390926081660651c338a62b
SaucisseSS <58030235+SaucisseSS@users.noreply.github.com>
2023-01-30 14:53:20 +0300

    added employment with workbooks

--- RFRExcelLib/Controllers/ExcelFileController.cs
+++ RFRExcelLib/Controllers/ExcelFileController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Excel = Microsoft.Office.Interop.Excel;
+﻿using Excel = Microsoft.Office.Interop.Excel;
 using System.Reflection;
 
 namespace RFRExelLib.Controllers
@@ -11,34 +6,43 @@ namespace RFRExelLib.Controllers
     public sealed class ExcelFileController
     {
         private Excel.Application _application;
-        private Excel.Workbook _workbooks;
+        private Excel.Workbook _activeWorkbook;
 
-        public string Workbook => _workbooks.FullName;
-
-        public ExcelFileController()
+        
+        public string Workbook => _activeWorkbook.Name;
+        public string Workbooks
         {
-            _application = new Excel.Application();
-            _workbooks = _application.Workbooks.Add(Missing.Value);
+            get
+            {
+                string names = "";
+                for (int i = 0; i < _application.Workbooks.Count; i++)
+                    names += _application.Workbooks.Item[i].Name + '\n';
+                return names;
+            }
         }
+
         public ExcelFileController(string path)
         {
             _application = new Excel.Application();
-            _workbooks = _application.Workbooks.Add(path);
         }
         public void AddWorkbook(string path)
         {
-            if (path != null) _workbooks = _application.Workbooks.Add(path);
-            else
-            {
-                var a = _application.Workbooks.Add(Missing.Value);
-                _workbooks = a;
-            }
+            if (path != null) _activeWorkbook = _application.Workbooks.Add(path);
+            else _activeWorkbook = _application.Workbooks.Add(Missing.Value);
         }
-        public void CloseWorkbook()
+
+        public bool FindWorkbook() => _application.FindFile();
+        public void CloseWorkbook(string filename)
         {
-
+            if (filename != null) _application.ActiveWorkbook.Close();
+            else _application.ActiveWorkbook.Close(filename);
         }
-
+       
+        ~ExcelFileController()
+        {
+            _application.Workbooks.Close();
+            _application.Quit();
+        }
         
     }
 }
